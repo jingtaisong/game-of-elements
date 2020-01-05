44cdf6112a964e09279685ebc5ee13cf9497d4d0
Taisong Jing <taisong@dyn-160-39-63-174.dyn.columbia.edu>
2020-01-04 23:44:57 -0500

    specify db connection host, username, and password

--- dbQuery.js
+++ dbQuery.js
@@ -1,5 +1,9 @@
 const {Pool} = require('pg');
 const pool = new Pool({
+    host: 'game-of-elements-db-instance.ct6nkwyja7yv.us-east-2.rds.amazonaws.com',
+    user: 'postgres',
+    password: 'Dirichlet1805',
+    // don't need above lines if develop locally; they are for AWS only
     database: 'game_of_elements'
 });
 
